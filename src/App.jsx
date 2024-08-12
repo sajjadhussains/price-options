8bcbb1ebc69938da2a009697592b4e5b5c4c5260
sajjadhussains <shuvosajjad70@gmail.com>
2024-08-13 04:48:39 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <h1 className="text-2xl text-center bg-green-500 text-red-500">
+        bangladesh 2.0
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+import Link from "../Link/Link";
+
+const Navbar = () => {
+  const routes = [
+    { id: 1, name: "Home", path: "/home" },
+    { id: 2, name: "About", path: "/about" },
+    { id: 3, name: "Services", path: "/services" },
+    { id: 4, name: "Contact", path: "/contact" },
+    { id: 5, name: "Blog", path: "/blog" },
+  ];
+
+  return (
+    <div>
+      <ul className="md:flex">
+        {routes.map((route) => (
+          <Link key={route.id} route={route}></Link>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
