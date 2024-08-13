d555adc229b1e3dd198fbac20e03e3bfb848af64
sajjadhussains <shuvosajjad70@gmail.com>
2024-08-13 13:37:33 +0600

    load by axios done

--- src/components/LoadByAxios/LoadByAxios.jsx
+++ src/components/LoadByAxios/LoadByAxios.jsx
@@ -1,13 +1,33 @@
 import { useState } from "react";
+import { BarChart, Bar } from "recharts";
 
 import axios from "axios";
 
 const LoadByAxios = () => {
-  //   const [data, setData] = useState([]);
+  const [phones, setPhones] = useState([]);
   axios
     .get("https://openapi.programming-hero.com/api/phones?search=iphone")
-    .then((data) => console.log(data.data.data));
-  return <div></div>;
+    .then((data) => {
+      const phoneData = data.data.data;
+      const phonesWithFakeData = phoneData.map((phone) => {
+        const obj = {
+          name: phone.phone_name,
+          price: parseInt(phone.slug.split("-")[1]),
+        };
+        // console.log(obj);
+        return obj;
+      });
+      //   console.log(phonesWithFakeData);
+      setPhones(phonesWithFakeData);
+      // console.log(phones.length);
+    });
+  return (
+    <div>
+      <BarChart width={600} height={400} data={phones}>
+        <Bar dataKey="price" fill="#8884d8" />
+      </BarChart>
+    </div>
+  );
 };
 
 export default LoadByAxios;
